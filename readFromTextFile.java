03aab278d6ff1e3deb7ca8bce40d0e68e63fb46e
bharathiboda <50348970+bharathiboda@users.noreply.github.com>
2019-11-24 13:03:13 -0500

    read1

--- readFromTextFile.java
+++ readFromTextFile.java
@@ -1,1 +1,20 @@
-public class 
+public class readFromTextFile{
+  File file = new File("path of the file");
+  file.CreateNewFile();
+  FileWriter fw = new FileWriter(file);
+  BufferedWriter bw = new BufferedWriter(fw);
+  //this is to write content to the file
+  bw.write("this is my first line");
+  bw.newLine();
+  bw.write("this is my second line");
+  bw.close();
+  // this is to read from the file 
+  FileReader fr = new FileReader(file);
+  BufferedReader br = new BufferedReader(fr);
+  String content = " ";
+  while ((content = br.readLine())!=null){
+    system.out.println(content);
+  }
+  br.close();
+}
+
