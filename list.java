77174090bb8e6827db3f6909ce2542ddba227df5
bharathiboda <50348970+bharathiboda@users.noreply.github.com>
2019-11-24 14:05:54 -0500

    Update list.java

--- list.java
+++ list.java
@@ -1,1 +1,21 @@
+public class listExcercise{
+  public static void main(String[] args){
+    WebDriver driver = new geckoDriver();
+    WebDriver driver1 = new ieDriver();
+    WebDriver driver2 = new ChromeDriver();
+    System.SetProperty("webdriver.chrome.driver","path of the exe file of chrome driver");
+    System.SetProperty("webdriver.gecko.driver","path of the exe file of gecko driver");
+    System.SetProperty("webdriver.ie.driver","path of the exe file of ie driver");
+    driver.get("the website url");
+    WebElement ele = driver.findElements(By.id("path"));
+    Select sel = new Select(ele)
+      sel.selectByValue("value");
+      sel.SelectByVisibleText("visibletext");
+      sel.SelectByIndex(i);
+    String data="";
+    for (int i=0;i<=ele.size();i++){
+      data = ele.get(i).getText();
+    }
+  }
 
+     
